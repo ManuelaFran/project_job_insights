afe7f79455617556a7326709ea6608708dfc7587
Manuela França <manuelaalves26@gmail.com>
2023-03-26 16:49:07 -0300

    teste função count_ocurrences.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+    assert count_ocurrences(path, 'Python') == 44
+    assert count_ocurrences(path, 'python') == 44
+    assert count_ocurrences(path, 'Javascript') == 22
+    assert count_ocurrences(path, 'javascript') == 22
