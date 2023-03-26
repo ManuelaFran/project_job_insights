907993d51e831463aa1ca51257a9100fab5ecfed
Manuela França <manuelaalves26@gmail.com>
2023-03-26 17:28:47 -0300

    fix: corrigindo teste da função count_ocurrences.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,7 +3,7 @@ from src.pre_built.counter import count_ocurrences
 
 def test_counter():
     path = 'data/jobs.csv'
-    assert count_ocurrences(path, 'Python') == 44
-    assert count_ocurrences(path, 'python') == 44
-    assert count_ocurrences(path, 'Javascript') == 22
-    assert count_ocurrences(path, 'javascript') == 22
+    assert count_ocurrences(path, 'Python') == 1639
+    assert count_ocurrences(path, 'python') == 1639
+    assert count_ocurrences(path, 'Javascript') == 122
+    assert count_ocurrences(path, 'javascript') == 122
